3ea47d8ae9a451febc3040d1eeda5c1d2bb35126
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-01-08 16:07:12 +0600

    login and register page refactor done

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useNavigate } from "react-router-dom";
-import SocialLogin from "../../components/ui/SocialLoing/SocialLogin";
+import Lottie from "lottie-react";
 import CustomForm from "../../components/From/CustomForm";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import toast from "react-hot-toast";
@@ -10,7 +11,9 @@ import { useAppDispatch } from "../../Redux/hook";
 import { setUser } from "../../Redux/Feature/Auth/authSlice";
 import { handleApiError } from "../../utils/handleApiError";
 import CustomOutlineButton from "../../components/ui/Button/CustomOutlineButton";
-
+import regAnimation from "../../assets/register (1).json";
+import { Button } from "antd";
+import { LuArrowLeft } from "react-icons/lu";
 const Login = () => {
   const navigate = useNavigate();
   const [login] = useLoginMutation();
@@ -34,73 +37,210 @@ const Login = () => {
       handleApiError(error, toastId);
     }
   };
+
+  const loginDemo = async (data: any) => {
+    const toastId = toast.loading("Register pending...");
+    try {
+      const res = await login(data).unwrap();
+      if (res.success) {
+        const userData = verifyToken(res?.data?.accessToken);
+        dispatch(
+          setUser({ user: userData?.data, token: res?.data?.accessToken })
+        );
+        navigate(`/`);
+        toast.success("Login Successfully done", {
+          id: toastId,
+          duration: 3000,
+        });
+      }
+    } catch (error) {
+      handleApiError(error, toastId);
+    }
+  };
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-[#1b1b1b]">
-      <div className="max-w-3xl  p-6 shadow-md sm:px-8 sm:py-10 lg:px-12 lg:py-16 ">
-        <div className="flex flex-col justify-between space-x-0 sm:flex-row sm:space-x-12">
-          <div className="mb-8 w-full sm:mb-0 sm:w-1/2">
-            {/* Left side form */}
-            <h2 className="mb-6 text-3xl font-semibold tracking-tight text-white">
-              Sign In
-            </h2>
-            <CustomForm onSubmit={onSubmit} >
-              <div className="  ">
-                <CustomInput
-                  name="email"
-                  label="Email"
-                  type="email"
-                  isLabelColor={true}
-                  placeholder={"abc@gmail.com"}
-                />
-                <CustomInput
-                  name="password"
-                  label="password"
-                  type="password"
-                  isLabelColor={true}
-                  placeholder={"password"}
-                />
-              </div>
-              <div className="mb-6 flex items-center space-x-2 accent-sky-600">
-                <input type="checkbox" id="keep_signed_in" />
-                <label
-                  className="select-none text-sm font-medium text-white"
-                  htmlFor="keep_signed_in"
+    <>
+      <div className="flex  flex-col md:flex-row md:flex md:justify-center gap-10 items-center min-h-screen bg-[#1b1b1b] text-white ">
+        <div className="space-y-6 rounded-lg  p-10 shadow-lg mt-5 w-screen sm:max-w-lg ">
+          {/* Left side form */}
+          <h2 className="mb-6 text-3xl font-bold tracking-tight light:text-lightText flex items-center gap-3">
+            <LuArrowLeft
+              onClick={() => navigate("/")}
+              className="cursor-pointer"
+            />{" "}
+            Sign In
+          </h2>
+
+          <div>
+            <div className="mb-6">
+              <h2 className="mb-3 text-blue-500">Login Demo Credential:</h2>
+              <div className="flex flex-wrap gap-2">
+                <Button
+                  size="small"
+                  type="dashed"
+                  className="bg-blue-500 hover:bg-blue-600"
+                  onClick={() =>
+                    loginDemo({
+                      email: "u1@gmail.com",
+                      password: "password123",
+                    })
+                  }
                 >
-                  Keep me signed in
-                </label>
+                  User Credentials
+                </Button>
+
+                <Button
+                  size="small"
+                  type="dashed"
+                  className="bg-blue-500 hover:bg-blue-600"
+                  onClick={() =>
+                    loginDemo({
+                      email: "a1@gmail.com",
+                      password: "password123",
+                    })
+                  }
+                >
+                  Admin Credentials
+                </Button>
               </div>
-             
-              <CustomOutlineButton name="Login" customCss="w-full"  textColor="black" isTransParent={false} />
-            </CustomForm>
-            <p className="mt-6 flex gap-1 text-sm text-white">
-              Did you
-              <span onClick={()=>navigate("/forget-password")} className="text-sky-500 underline cursor-pointer" >
-                forget your password?
-              </span>
-            </p>
-          </div>
-          {/* Right side content */}
-          <div className="w-full sm:w-1/2">
-            <p className="mb-6 text-sm text-white">
-              If you don&apos;t already have an account click the button below
-              to create your account.
-            </p>
-            <button
-              onClick={() => {
-                navigate("/register");
-              }}
-              className="mb-2 inline-flex h-10 w-full items-center justify-center rounded-md bg-zinc-800 px-4 py-2 text-sm font-medium uppercase text-white hover:bg-zinc-700"
-            >
-              Create Account
-            </button>
-            <p className="my-4 text-center text-white">OR</p>
-            <div>
-              <SocialLogin />
             </div>
+          </div>
+
+          <CustomForm onSubmit={onSubmit}>
+            <div className="  ">
+              <CustomInput
+                name="email"
+                label="Email"
+                type="email"
+                isLabelColor={true}
+                placeholder={"abc@gmail.com"}
+              />
+              <CustomInput
+                name="password"
+                label="password"
+                type="password"
+                isLabelColor={true}
+                placeholder={"password"}
+              />
+            </div>
+
+            <CustomOutlineButton
+              name="Login"
+              customCss="w-full"
+              textColor="black"
+              isTransParent={false}
+            />
+          </CustomForm>
+          <p className="mt-6 flex gap-1 text-sm text-white">
+            Did you
+            <span
+              onClick={() => navigate("/forget-password")}
+              className="text-sky-500 underline cursor-pointer"
+            >
+              forget your password?
+            </span>
+          </p>
+          <div className="text-center mt-4 ">
+            <span>
+              <span className="light:text-lightText">
+                If you don&apos;t already have an account click the button below
+                to create your account.
+              </span>{" "}
+              <span
+                onClick={() => {
+                  navigate("/register");
+                }}
+                className="text-sky-500 underline hover:cursor-pointer"
+              >
+                ?Register
+              </span>{" "}
+            </span>
+          </div>
+        </div>
+
+        {/* Right side content */}
+        <div>
+          <div>
+            <Lottie
+              animationData={regAnimation}
+              loop={true}
+              autoplay={true}
+            ></Lottie>
           </div>
         </div>
       </div>
-    </div>
+    </>
+    // <div className="flex justify-center items-center h-screen w-screen bg-[#1b1b1b]">
+    //   <div className="max-w-3xl  p-6 shadow-md sm:px-8 sm:py-10 lg:px-12 lg:py-16 ">
+    //     <div className="flex flex-col justify-between space-x-0 sm:flex-row sm:space-x-12">
+    //       <div className="mb-8 w-full sm:mb-0 sm:w-1/2">
+    //         {/* Left side form */}
+    //         <h2 className="mb-6 text-3xl font-semibold tracking-tight text-white">
+    //           Sign In
+    //         </h2>
+    //         <CustomForm onSubmit={onSubmit}>
+    //           <div className="  ">
+    //             <CustomInput
+    //               name="email"
+    //               label="Email"
+    //               type="email"
+    //               isLabelColor={true}
+    //               placeholder={"abc@gmail.com"}
+    //             />
+    //             <CustomInput
+    //               name="password"
+    //               label="password"
+    //               type="password"
+    //               isLabelColor={true}
+    //               placeholder={"password"}
+    //             />
+    //           </div>
+
+    //           <CustomOutlineButton
+    //             name="Login"
+    //             customCss="w-full"
+    //             textColor="black"
+    //             isTransParent={false}
+    //           />
+    //         </CustomForm>
+    //         <p className="mt-6 flex gap-1 text-sm text-white">
+    //           Did you
+    //           <span
+    //             onClick={() => navigate("/forget-password")}
+    //             className="text-sky-500 underline cursor-pointer"
+    //           >
+    //             forget your password?
+    //           </span>
+    //         </p>
+    //       </div>
+    //       {/* Right side content */}
+    //       <div className="w-full sm:w-1/2">
+    //         <p className="mb-6 text-sm text-white">
+    //           If you don&apos;t already have an account click the button below
+    //           to create your account.
+    //         </p>
+    //         <button
+    //           onClick={() => {
+    //             navigate("/register");
+    //           }}
+    //           className="mb-2 inline-flex h-10 w-full items-center justify-center rounded-md bg-zinc-800 px-4 py-2 text-sm font-medium uppercase text-white hover:bg-zinc-700"
+    //         >
+    //           Create Account
+    //         </button>
+    //         <p className="my-4 text-center text-white">OR</p>
+    //         {/* Right side content */}
+    //         <div>
+    //           <div>
+    //             <Lottie
+    //               animationData={regAnimation}
+    //               loop={true}
+    //               autoplay={true}
+    //             ></Lottie>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -13,6 +13,9 @@ import { setUser } from "../../Redux/Feature/Auth/authSlice";
 import verifyToken from "../../utils/verifyToken";
 import { handleApiError } from "../../utils/handleApiError";
 import CustomOutlineButton from "../../components/ui/Button/CustomOutlineButton";
+import Lottie from "lottie-react";
+import regAnimation from "../../assets/register (1).json";
+import { LuArrowLeft } from "react-icons/lu";
 
 const Register = () => {
   const navigate = useNavigate();
@@ -43,96 +46,197 @@ const Register = () => {
     }
   };
   return (
-    <div className="flex justify-center items-center min-h-screen   bg-pageBg">
-      <div className=" space-y-6 rounded-lg dark:bg-zinc-900 p-10 shadow-lg mt-5 w-screen sm:max-w-lg">
-        <div className="flex flex-col space-y-1">
-          <h3 className="text-3xl font-bold tracking-tight text-white">
-            Sign Up
-          </h3>
-          <p className="text-sm text-zinc-500 dark:text-zinc-400">
-            Please fill in the form to create an account.
-          </p>
-        </div>
+    <>
+      <div className="flex  flex-col-reverse md:flex-row md:flex md:justify-center gap-4 items-center min-h-screen light:text-lightText bg-pageBg text-white">
         <div>
-          <CustomForm
-            onSubmit={onSubmit}
-            resolver={zodResolver(registerSchema)}
-          >
-            <div>
-              <CustomInput
-                name="name"
-                label="Name"
-                type="text"
-                isLabelColor={true}
-              />
-            </div>
-            <div>
-              <CustomInput
-                name="email"
-                label="Email"
-                type="email"
-                isLabelColor={true}
-                placeholder={"abc@gmail.com"}
-              />
-            </div>
-            <div>
-              <CustomInput
-                name="phone"
-                label="Phone"
-                type="string"
-                isLabelColor={true}
-                placeholder={"Enter your phone number"}
-              />
-            </div>
-            {/* password  */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
-              <div>
-                <CustomInput
-                  name="password"
-                  label="password"
-                  type="password"
-                  isLabelColor={true}
-                  placeholder={"password"}
-                />
-              </div>
-              <div>
-                <CustomInput
-                  name="confirmPassword"
-                  label="Confirm Password"
-                  type="password"
-                  isLabelColor={true}
-                  placeholder={"password"}
-                />
-              </div>
-            </div>
-            <CustomOutlineButton
-              name="Sign up"
-              customCss="w-full"
-              textColor="black"
-              isTransParent={false}
-            />
-          </CustomForm>
-          <div className="text-center mt-4 ">
-            <span>
-              <span className="text-white">Already have an account</span>{" "}
-              <span
-                onClick={() => {
-                  navigate("/login");
-                }}
-                className="text-sky-500 underline hover:cursor-pointer"
-              >
-                ?Signin
-              </span>{" "}
-            </span>
-          </div>
+          <Lottie
+            animationData={regAnimation}
+            loop={true}
+            autoplay={true}
+          ></Lottie>
+        </div>
 
-          {/* social login  */}
-          <div className="mt-5">
-            <SocialLogin />
+        <div className=" space-y-6 rounded-lg  p-10 shadow-lg mt-5 w-screen sm:max-w-lg">
+          <div className="flex flex-col space-y-1">
+            <h3 className="text-3xl font-bold tracking-tight text-white flex items-center gap-3">
+              <LuArrowLeft
+                onClick={() => navigate("/login")}
+                className="cursor-pointer"
+              />{" "}
+              Register
+            </h3>
+            <p className="text-sm light:text-lightText">
+              Please fill in the form to create an account.
+            </p>
+          </div>
+          <div>
+            <CustomForm
+              onSubmit={onSubmit}
+              resolver={zodResolver(registerSchema)}
+            >
+              <div>
+                <CustomInput
+                  name="name"
+                  label="Name"
+                  type="text"
+                  isLabelColor={true}
+                />
+              </div>
+              <div>
+                <CustomInput
+                  name="email"
+                  label="Email"
+                  type="email"
+                  isLabelColor={true}
+                  placeholder={"abc@gmail.com"}
+                />
+              </div>
+              <div>
+                <CustomInput
+                  name="phone"
+                  label="Phone"
+                  type="string"
+                  isLabelColor={true}
+                  placeholder={"Enter your phone number"}
+                />
+              </div>
+              {/* password  */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+                <div>
+                  <CustomInput
+                    name="password"
+                    label="password"
+                    type="password"
+                    isLabelColor={true}
+                    placeholder={"password"}
+                  />
+                </div>
+                <div>
+                  <CustomInput
+                    name="confirmPassword"
+                    label="Confirm Password"
+                    type="password"
+                    isLabelColor={true}
+                    placeholder={"password"}
+                  />
+                </div>
+              </div>
+              <CustomOutlineButton
+                name="Sign up"
+                customCss="w-full"
+                textColor="black"
+                isTransParent={false}
+              />
+            </CustomForm>
+            <div className="text-center mt-4 ">
+              <span>
+                <span className="light:text-lightText">
+                  Already have an account
+                </span>{" "}
+                <span
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                  className="text-sky-500 underline hover:cursor-pointer"
+                >
+                  ?Signin
+                </span>{" "}
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
+    // <div className="flex justify-center items-center min-h-screen   bg-pageBg">
+    //   <div className=" space-y-6 rounded-lg dark:bg-zinc-900 p-10 shadow-lg mt-5 w-screen sm:max-w-lg">
+    //     <div className="flex flex-col space-y-1">
+    //       <h3 className="text-3xl font-bold tracking-tight text-white">
+    //         Sign Up
+    //       </h3>
+    //       <p className="text-sm text-zinc-500 dark:text-zinc-400">
+    //         Please fill in the form to create an account.
+    //       </p>
+    //     </div>
+    //     <div>
+    //       <CustomForm
+    //         onSubmit={onSubmit}
+    //         resolver={zodResolver(registerSchema)}
+    //       >
+    //         <div>
+    //           <CustomInput
+    //             name="name"
+    //             label="Name"
+    //             type="text"
+    //             isLabelColor={true}
+    //           />
+    //         </div>
+    //         <div>
+    //           <CustomInput
+    //             name="email"
+    //             label="Email"
+    //             type="email"
+    //             isLabelColor={true}
+    //             placeholder={"abc@gmail.com"}
+    //           />
+    //         </div>
+    //         <div>
+    //           <CustomInput
+    //             name="phone"
+    //             label="Phone"
+    //             type="string"
+    //             isLabelColor={true}
+    //             placeholder={"Enter your phone number"}
+    //           />
+    //         </div>
+    //         {/* password  */}
+    //         <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+    //           <div>
+    //             <CustomInput
+    //               name="password"
+    //               label="password"
+    //               type="password"
+    //               isLabelColor={true}
+    //               placeholder={"password"}
+    //             />
+    //           </div>
+    //           <div>
+    //             <CustomInput
+    //               name="confirmPassword"
+    //               label="Confirm Password"
+    //               type="password"
+    //               isLabelColor={true}
+    //               placeholder={"password"}
+    //             />
+    //           </div>
+    //         </div>
+    //         <CustomOutlineButton
+    //           name="Sign up"
+    //           customCss="w-full"
+    //           textColor="black"
+    //           isTransParent={false}
+    //         />
+    //       </CustomForm>
+    //       <div className="text-center mt-4 ">
+    //         <span>
+    //           <span className="text-white">Already have an account</span>{" "}
+    //           <span
+    //             onClick={() => {
+    //               navigate("/login");
+    //             }}
+    //             className="text-sky-500 underline hover:cursor-pointer"
+    //           >
+    //             ?Signin
+    //           </span>{" "}
+    //         </span>
+    //       </div>
+
+    //       {/* social login  */}
+    //       <div className="mt-5">
+    //         <SocialLogin />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
